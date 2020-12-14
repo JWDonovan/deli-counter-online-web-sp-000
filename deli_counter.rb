75617ab1717a02a95f1c3d23f11e3c0125d0f2a7
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 20:11:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,12 @@
 def line(deli)
   if deli.count == 0
     puts 'The line is currently empty.'
+  else
+    deli.collect do |person|
+
+    end
+
+    deli.unshift('The line is currently: ')
+    puts deli
   end
 end
