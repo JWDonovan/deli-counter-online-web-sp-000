91fcc525602a03212292020f3d057c11f181da39
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 20:15:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,24 @@ def line(deli)
   if deli.count == 0
     puts 'The line is currently empty.'
   else
-    deli.each_with_index do |person, index|
-      deli[person] = " #{index + 1}. #{person}"
-    end
+    str = 'The line is currently: '
 
-    deli.unshift('The line is currently:')
-    puts deli.join()
+    deli.each_with_index do |person, index|
+      str += "#{index + 1}. #{person}"
+    end
   end
 end
+
+} else {
+    let string = 'The line is currently: ';
+
+    for (let i = 0; i < line.length; i++) {
+      string += `${i + 1}. ${line[i]}`;
+
+      if (i !== line.length - 1) {
+        string += ', ';
+      }
+    }
+
+    return string;
+  }
