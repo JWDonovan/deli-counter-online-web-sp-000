d399a79c7a40daf9818c78adcc198fe0a7520d2d
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 20:16:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(deli)
   if deli.count == 0
     puts 'The line is currently empty.'
   else
-    str = 'The line is currently: '
+    str = 'The line is currently:'
 
     deli.each_with_index do |person, index|
-      str += "#{index + 1}. #{person}"
+      str += " #{index + 1}. #{person}"
     end
 
     return str
